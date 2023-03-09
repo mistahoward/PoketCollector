00d7ca0493fbaec8cc30727bb4dc737fa029b43d
mistahoward <me@alexhoward.dev>
2023-03-09 00:11:59 -0600

    switching login to thunk, fixing login finally :)

--- front-end/src/store/user/api.ts
+++ front-end/src/store/user/api.ts
@@ -1,8 +1,20 @@
-import { UserCreationPayload } from './types';
+import { SuccessOrError } from '../types';
+import { UserCreationPayload, UserLoginPayload } from './types';
 
 const userApi = {
-	registerUser: async (user: UserCreationPayload) => {
+	registerUser: async (user: UserCreationPayload): SuccessOrError<UserStore => {
 		const response = await fetch(`${import.meta.env.VITE_BACKEND_ROOT}/user/register`, {
+			method: 'POST',
+			body: JSON.stringify(user),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		const data = await response.json();
+		return data;
+	},
+	loginUser: async (user: UserLoginPayload) => {
+		const response = await fetch(`${import.meta.env.VITE_BACKEND_ROOT}/user/login`, {
 			method: 'POST',
 			body: JSON.stringify(user),
 			headers: {
